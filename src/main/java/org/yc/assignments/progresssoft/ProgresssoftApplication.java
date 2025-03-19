4df51887926438845d18f08013b57044ea0e1fbc
Mushi <144794503+Mouche00@users.noreply.github.com>
2025-03-19 11:08:59 +0000

    [#1] setup: initialize the spring boot application

---
+++ src/main/java/org/yc/assignments/progresssoft/ProgresssoftApplication.java
@@ -0,0 +1,13 @@
+package org.yc.assignments.progresssoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgresssoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgresssoftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/yc/assignments/progresssoft/ProgresssoftApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yc.assignments.progresssoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgresssoftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
