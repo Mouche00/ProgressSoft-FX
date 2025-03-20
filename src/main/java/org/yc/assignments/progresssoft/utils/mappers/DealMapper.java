4fe3775d358085a6bee8c03d290cf68bf701cb0b
Mushi <144794503+Mouche00@users.noreply.github.com>
2025-03-20 19:32:47 +0000

    [#9] feature: add additional methods to mapper. Closes #9

--- src/main/java/org/yc/assignments/progresssoft/utils/mappers/DealMapper.java
+++ src/main/java/org/yc/assignments/progresssoft/utils/mappers/DealMapper.java
@@ -5,9 +5,13 @@ import org.yc.assignments.progresssoft.dtos.DealRequestDTO;
 import org.yc.assignments.progresssoft.dtos.DealResponseDTO;
 import org.yc.assignments.progresssoft.models.Deal;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface DealMapper {
     DealResponseDTO dealToResponseDTO(Deal deal);
     Deal requestDTOToDeal(DealRequestDTO dealRequestDTO);
+    List<DealResponseDTO> dealsToResponseDTOs(List<Deal> deals);
+    List<Deal> requestDTOsToDeals(List<DealRequestDTO> dealRequestDTOs);
 
 }
